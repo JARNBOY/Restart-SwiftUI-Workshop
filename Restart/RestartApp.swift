6b5032f6b62bd1ae114c1331fb61d837240ad824
Papon <papon.s@lawyerplusgroup.com>
2023-06-19 11:12:06 +0700

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Papon Supamongkonchai on 19/6/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

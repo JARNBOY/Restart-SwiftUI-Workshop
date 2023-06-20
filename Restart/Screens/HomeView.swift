fc80028482a43ec11ee461156d795448155cb540
Papon <papon.s@lawyerplusgroup.com>
2023-06-20 09:52:12 +0700

    [RESTART Workshop] build Home Screen , and reuse view of CircleGroupView (Reviewed by Papon S.)

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -12,15 +12,43 @@ struct HomeView: View {
     
     var body: some View {
         VStack(spacing: 20) {
-            Text("Home")
-                .font(.largeTitle)
+            
+            //MARK: HEADER
+            Spacer()
+            
+            ZStack {
+                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+                
+                Image("character-2").imageModifier()
+                    .padding()
+            }
+            
+            //MARK: CENTER
+            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            //MARK: FOOTER
+            
+            Spacer()
             
             Button {
                 //Some Action
                 isOnboardingViewActive = true
             } label: {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                
                 Text("Restart")
+                    .font(.title3)
+                    .fontWeight(.bold)
             }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
 
         }
     }
